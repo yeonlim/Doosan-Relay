4f381400cbcfda61a55b1a6b3897994c2e5d0c68
yeonlim <yeonlimj@gmail.com>
2024-11-22 15:33:58 +0900

    Initial Commit

--- src/main/java/com/daeu/doosan/io/IF_SFDC_KODATA_ENP_INFO/IF_SFDC_KODATA_ENP_INFO_Req.java
+++ src/main/java/com/daeu/doosan/io/IF_SFDC_KODATA_ENP_INFO/IF_SFDC_KODATA_ENP_INFO_Req.java
@@ -2,8 +2,7 @@ package com.daeu.doosan.io.IF_SFDC_KODATA_ENP_INFO;
 
 public class IF_SFDC_KODATA_ENP_INFO_Req {
     private String userid;      //아이디 
-
-    private String process;     //처리구분 
+    private String process;     //처리구분
     private String bzno;        //요청사업자번호 
     private String pidagryn;    //개인정보조회동의여부  - Y / N
     private String jmno;        //전문상세코드
--- src/main/java/com/daeu/doosan/io/IF_SFDC_KODATA_ENP_SEARCH/IF_SFDC_KODATA_ENP_SEARCH_Req.java
+++ src/main/java/com/daeu/doosan/io/IF_SFDC_KODATA_ENP_SEARCH/IF_SFDC_KODATA_ENP_SEARCH_Req.java
@@ -2,7 +2,6 @@ package com.daeu.doosan.io.IF_SFDC_KODATA_ENP_SEARCH;
 
 public class IF_SFDC_KODATA_ENP_SEARCH_Req {
     private String userid;      // 기관 ID
-
     private String jmno;        // 기관서비스전문번호
     private String enpnm;       // 기업명
     private String page;        // 페이지 수
